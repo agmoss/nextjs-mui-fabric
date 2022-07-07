03c0e520e81a43bdaa98e5db30579348e4a6a743
Andrew Moss <andrew@m0ss.dev>
2022-07-07 10:54:08 -0600

    feat: Creating initial next project

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+
+import createEmotionCache from '../src/util/createEmotionCache';
+import lightThemeOptions from '../src/theme/lightThemeOptions';
+interface _AppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const clientSideEmotionCache = createEmotionCache();
+
+const lightTheme = createTheme(lightThemeOptions);
+
+const _App: React.FunctionComponent<_AppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default _App;
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>nextjs-mui-fabric</title>
+        <meta name="description" content="nextjs-mui-fabric" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>{'nextjs-mui-fabric'}</p>
+      </main>
+      <footer>
+        <p>{'nextjs-mui-fabric'}</p>
+      </footer>
+    </>
+  );
+};
+
+export default Home;
